a10a9f4758d17e77c6ea7b3dd00b71813387eb6a
darchi07 <kokuryousei@gmail.com>
2018-09-12 22:44:35 +0900

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	def hello
+		render html: "Hello World!"
+	end
+
+	def bye
+		render html: "Good bye;;"
+	end	
+	
+
+end
